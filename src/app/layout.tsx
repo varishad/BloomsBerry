845f7cbd2a80457072a63641fa05a667230c89e3
FahimShahrier32 <FahimShahrier32@gmail.com>
2026-02-24 16:16:51 +0600

    Add new favicon assets and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,10 @@ export default function RootLayout({
     <html lang="en" className={`${poppins.variable} ${playfair.variable}`} suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#2D3A2F" />
       </head>
       <body suppressHydrationWarning>
